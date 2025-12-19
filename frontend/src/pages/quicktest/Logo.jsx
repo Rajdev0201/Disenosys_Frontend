c5dd53941f8d9625c3e2dde4f2b873bea5ee1fe0
Rajkumarp02 <rajkumar.p@techjays.com>
2025-12-19 18:08:28 +0530

    ""

--- frontend/src/pages/quicktest/Logo.jsx
+++ frontend/src/pages/quicktest/Logo.jsx
@@ -3,10 +3,14 @@ import React from 'react'
 import Image from "next/image";
 import logo from "@/components/assests/logo.jpg"
 
-export const Logo = () => {
+
+ const Logo = () => {
   return (
     <div className='shadow-sm bg-white fixed w-full top-0 left-0 right-0 p-3 z-50 flex justify-center items-center'>
         <Image src={logo} className='object-cover w-44' alt='brand-logo'/>
     </div>
   )
 }
+
+
+export default Logo;
